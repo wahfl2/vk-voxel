a37c2c63d1f36a693993e2066d73cb3ebaab1bc8
wahfl2 <wahflbeljihn@gmail.com>
2023-04-12 12:53:56 -0500

    various tests and slight refactor

--- src/world/generation/transformer.rs
+++ src/world/generation/transformer.rs
@@ -1,9 +1,9 @@
 use std::collections::hash_map::Entry;
 
 use ahash::{HashMap, HashMapExt};
-use ultraviolet::{UVec2, IVec2};
+use ultraviolet::{UVec2, IVec2, Vec2};
 
-use crate::util::util::UVecToSigned;
+use crate::util::util::{UVecToSigned, VecRounding, VecTrunc};
 
 use super::terrain::TerrainChunk;
 
@@ -39,9 +39,9 @@ where T: Clone
 
     pub fn apply(&mut self, chunk: &mut TerrainChunk) {
         let locations = self.locations(chunk.blocks.pos);
-        let offsets = locations.clone().into_iter().map(|v| { v * self.spacing.signed() }).collect::<Vec<_>>();
-        let pos = chunk.blocks.pos;
-        println!("chunk ({}, {}), offsets: {:?}", pos.x, pos.y, offsets);
+        // let offsets = locations.clone().into_iter().map(|v| { v * self.spacing.signed() }).collect::<Vec<_>>();
+        // let pos = chunk.blocks.pos;
+        // println!("chunk ({}, {}), offsets: {:?}", pos.x, pos.y, offsets);
 
         for location in locations {
             let data = &*match self.data_cache.entry(location) {
@@ -55,23 +55,96 @@ where T: Clone
     }
 
     fn locations(&self, chunk_pos: IVec2) -> Vec<IVec2> {
-        let minus_1 = self.size - UVec2::one();
-        let iv = IVec2::new(minus_1.x as i32, minus_1.y as i32);
-        let check_min = chunk_pos - iv;
-
-        let mut ret = Vec::new();
+        let min = chunk_pos - (self.size - UVec2::one()).signed();
         let i_spacing = self.spacing.signed();
-
-        for chunk_x in check_min.x..=chunk_pos.x {
-            if (chunk_x % i_spacing.x) == 0 {
-                for chunk_y in check_min.y..=chunk_pos.y {
-                    if (chunk_y % i_spacing.y) == 0 {
-                        ret.push(IVec2::new(chunk_x, chunk_y) / i_spacing);
+        let mut ret = Vec::new();
+        for x in min.x..=chunk_pos.x {
+            if x % i_spacing.x == 0 {
+                for y in min.y..=chunk_pos.y {
+                    if y % i_spacing.y == 0 {
+                        ret.push(IVec2::new(x, y) / self.spacing.signed());
                     }
                 }
             }
         }
 
         ret
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn transformer() {
+        fn expect(transformer: TerrainTransformer<()>, expected: Vec<IVec2>, chunk_pos: IVec2) {
+            let mut actual = transformer.locations(chunk_pos);
+            for e in expected {
+                let i = actual.index_of(&e).expect(&format!("Actual did not contain {:?},\nActual: {:?}", e, actual));
+                actual.swap_remove(i);
+            }
+    
+            if !actual.is_empty() {
+                panic!("Actual contained extraneous elements: {:?}", actual);
+            }
+        }
+
+        expect(
+            TerrainTransformer::new(
+                UVec2::new(2, 2),
+                UVec2::new(1, 1),
+                |_, _| {}, |_, _, _, _| {}
+            ), 
+            vec![
+                IVec2::new(-1, -1),
+                IVec2::new(0, -1),
+                IVec2::new(-1, 0),
+                IVec2::new(0, 0),
+            ], 
+            IVec2::zero(),
+        );
+
+        expect(
+            TerrainTransformer::new(
+                UVec2::new(3, 3),
+                UVec2::new(1, 1),
+                |_, _| {}, |_, _, _, _| {}
+            ), 
+            vec![
+                IVec2::new(-2, -2), IVec2::new(-2, -1), IVec2::new(-2, 0),
+                IVec2::new(-1, -2), IVec2::new(-1, -1), IVec2::new(-1, 0),
+                IVec2::new(0, -2), IVec2::new(0, -1), IVec2::new(0, 0),
+            ], 
+            IVec2::zero(),
+        );
+
+        expect(
+            TerrainTransformer::new(
+                UVec2::new(3, 3),
+                UVec2::new(2, 2),
+                |_, _| {}, |_, _, _, _| {}
+            ), 
+            vec![
+                IVec2::new(-1, -1), IVec2::new(-1, 0),
+                IVec2::new(0, -1), IVec2::new(0, 0),
+            ], 
+            IVec2::zero(),
+        );
+    }
+
+    trait IndexOf<T> {
+        fn index_of(&self, x: &T) -> Option<usize>;
+    }
+
+    impl<T> IndexOf<T> for Vec<T> 
+    where T: PartialEq
+    {
+        fn index_of(&self, x: &T) -> Option<usize> {
+            for (i, e) in self.iter().enumerate() {
+                if e == x { return Some(i) }
+            }
+            None
+        }
     }
 }
